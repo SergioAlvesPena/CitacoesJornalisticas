83491f38ecd954cf1fbb0d2da7ffa83b06e6b61c
sergio.pena2000@outlook.com <sergio.pena2000@outlook.com>
2021-05-13 00:27:58 -0300

    this is a mess but at least is working

--- CitacoesJornalisticas/CitacoesJornalisticas/Controllers/JornalGloboController.cs
+++ CitacoesJornalisticas/CitacoesJornalisticas/Controllers/JornalGloboController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,6 +8,7 @@ using System.Threading.Tasks;
 using HtmlAgilityPack;
 using System.Text;
 using System.Text.RegularExpressions;
+using CitacoesJornalisticas.Controllers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -33,8 +33,9 @@ namespace CitacoesJornalisticas.Controllers
             var WebRequisition = WebRequest.CreateHttp("https://g1.globo.com/busca/?q=jair+messias+bolsonaro&page=1");
             WebRequisition.Method = "GET";
             WebRequisition.UserAgent = "RequisicaoWebDemo";
+            List<string> bora = new List<string>();
 
-            WebResponse requisitionAnswer =  WebRequisition.GetResponse();
+            WebResponse requisitionAnswer = WebRequisition.GetResponse();
             Stream streamDados = requisitionAnswer.GetResponseStream();
             StreamReader reader = new StreamReader(streamDados);
             string objResponse = reader.ReadToEnd();
@@ -42,13 +43,26 @@ namespace CitacoesJornalisticas.Controllers
             doc.LoadHtml(objResponse);
             HtmlNode quotes = doc.GetElementbyId("content");
             string ListOfPosts = quotes.OuterHtml.Replace(@"\n", "breakalinehere");
-            ListOfPosts = ListOfPosts.Replace(@"\","");
+            ListOfPosts = ListOfPosts.Replace(@"\", "");
             System.IO.File.WriteAllText(@"C:\Users\Sergio Pena\Desktop\Unip\TCC\SergioPena\CitacoesJornalisticas\CitacoesJornalisticas\CitacoesJornalisticas\FileAccess\result.cshtml", ListOfPosts, Encoding.UTF8);
             int i = 0;
 
             Regex ListItemPattern = new Regex("(<li (class=\"widget widget--card widget--navigational\") (data-position=\"([0-9])\")>)+");
             List<string> itens = new List<string>();
-            itens.AddRange(ListOfPosts.Split(" "));
+            itens.AddRange(ListOfPosts.Split(@"href="));
+
+            
+            foreach (string link in itens)
+            {
+                int inicio = 0;
+                int final = 0;
+                if (link.Contains("g1.globo.com/busca"))
+                {
+                    inicio = link.IndexOf("g1");
+                    final = link.IndexOf("\">");
+                    bora.Add(link.Substring(inicio, final));
+                }
+            }
 
             while (ListOfPosts.Contains("data-position=\"" + i + "\""))
             {
